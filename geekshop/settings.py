afcdf65fb6327554f8f44b1546ed5149dd77be7a
Nella <nella611@yandex.ru>
2021-10-13 17:55:55 +0200

    lesson3

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products',
+    'products.apps.ProductsConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,6 +123,8 @@ STATICFILES_DIRS = (
     BASE_DIR / 'static',
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
