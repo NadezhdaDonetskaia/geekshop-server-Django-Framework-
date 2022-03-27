5717d63b8e7423a80c2e8f31850614ecb39fbdd3
Nella <nella611@yandex.ru>
2022-03-27 15:53:25 +0200

    added context

--- users/views.py
+++ users/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import auth, messages
@@ -26,11 +28,19 @@ def login(request):
 
 
 def registration(request):
+
+    def send_verify_link(user):
+        verify_link = reverse('user:verify', args=[user.email, user.activation_key])
+        subject = f'Для активации пользователя {user.username} пройдите по ссылке'
+        message = f'Для подтверждения учетной записи {user.username} на портале\n' \
+                  f'{settings.DOMAIN_NAME} пройдите по ссылке {settings.DOMAIN_NAME}{verify_link}'
+        return send_mail(subject, message, settings.EMAIL_HOST_USER, [user.email], fail_silently=False)
+
     if request.method == 'POST':
-        form = UserRegistrationForm(data=request.POST)
+        form = UserRegistrationForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Вы успешно зарегистрировались!')
+            user_reg = form.save()
+            send_verify_link(user_reg)
             return HttpResponseRedirect(reverse('users:login'))
     else:
         form = UserRegistrationForm()
@@ -44,6 +54,22 @@ def registration(request):
 def logout(request):
     auth.logout(request)
     return HttpResponseRedirect(reverse('index'))
+
+
+def verify(request, email, activate_key):
+    try:
+        user = UserLoginForm.objects.get(email=email)
+        if user and user.activation_key == activate_key and not user.is_activation_key_expired:
+            user.activation_key = ''
+            user.activation_key_expires = None
+            user.is_active = True
+            user.save(update_fields=['activation_key', 'activation_key_expires', 'is_active'])
+            auth.login(request, user)
+            return render(request, 'user/verification.html')
+    except Exception:
+        pass
+    else:
+        return render(request, 'user/verification.html')
 
 
 @login_required
